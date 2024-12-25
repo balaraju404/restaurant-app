248f4833f73738b3f9b09451744741c4bfa6f912
balaraju <balarajugandham1@gmail.com>
2024-12-25 23:28:56 +0530

    new icon changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,20 +1,27 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.gbr-food.app',
-  appName: 'GBR Food App',
-  webDir: 'www',
-  // server: {
-  //   androidScheme: 'https',
-  //   iosScheme: 'https',
-  //   hostname: 'your-backend-url.com'  // if using a custom backend URL
-  // }
-  "plugins": {
-    "Camera": {
-      "allowEditing": true,
-      "saveToGallery": false
-    }
+ appId: 'com.gbr-food.app',
+ appName: 'GBR Food App',
+ webDir: 'www',
+ // server: {
+ //   androidScheme: 'https',
+ //   iosScheme: 'https',
+ //   hostname: 'your-backend-url.com'  // if using a custom backend URL
+ // }
+ "plugins": {
+  "Camera": {
+   "allowEditing": true,
+   "saveToGallery": false
+  },
+  SplashScreen: {
+   launchShowDuration: 3000,
+   launchAutoHide: false,     
+   backgroundColor: "#ffffff", 
+   splashFullScreen: true,    
+   splashImmersive: true,  
   }
+ }
 
 };
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@capacitor/splash-screen';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+ selector: 'app-root',
+ templateUrl: 'app.component.html',
+ styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+ constructor(private platform: Platform) {
+  this.initializeApp();
+ }
+
+ initializeApp() {
+  this.platform.ready().then(() => {
+   SplashScreen.show();
+
+   setTimeout(() => {
+    SplashScreen.hide();
+   }, 3000);
+  });
+ }
 }
